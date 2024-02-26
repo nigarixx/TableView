9f3376ff363ecfcd476bfb7deaa6b6e4789aba85
nigarixx <alaskarovanigar@gmail.com>
2024-02-26 20:33:02 +0400

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Nigar Alaskarova on 26.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
